2257a2d0bbd9175ac8a6d10a12a0c536a82fc57f
Jennifer <jnmurrin@gmail.com>
2021-08-01 11:05:05 -0500

    add firebase config

---
+++ firebase.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+  apiKey: "AIzaSyCAF8rsd3PmJmkkQBuHnkIe-GMAISnt8oE",
+  authDomain: "clone-v2-2e4bf.firebaseapp.com",
+  projectId: "clone-v2-2e4bf",
+  storageBucket: "clone-v2-2e4bf.appspot.com",
+  messagingSenderId: "917596841717",
+  appId: "1:917596841717:web:fd00b4d3438efff982c7bc",
+  measurementId: "G-E9PPJ1JKRF",
+}
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+firebase.analytics()
